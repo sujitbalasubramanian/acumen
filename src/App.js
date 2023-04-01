2190e55c3585b07b8fec9c55c49f7c4302f16a8a
Sujit Balasubramanian <sujitbalasubramanianr@gmail.com>
2023-04-01 14:46:25 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom';
 import About from "./Pages/About";
 import Home from './Pages/Home'
 import Dev from './Pages/Dev'
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/about' element={<About />}></Route>
         <Route path='/dev' element={<Dev />}></Route>
         <Route path='/home' element={<Home />}></Route>
         <Route path='/' element={<Home />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
